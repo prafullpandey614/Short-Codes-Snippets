d8eb7c95907a9c476c2978483c64b2f98bef568e
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2022-07-03 16:45:07 +0530

    Create Reverse_a_Linked_List.cpp

---
+++ Reverse_a_Linked_List.cpp
@@ -0,0 +1,11 @@
+node* reverse(node* ptr) {
+    node* pre=NULL;
+    node* nex=NULL;
+    while(ptr!=NULL) {
+        nex = ptr->next;
+        ptr->next = pre;
+        pre=ptr;
+        ptr=nex;
+    }
+    return pre;
+}
